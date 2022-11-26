9063fc3e0cde223063a0d567b4af7bd8049f3123
sharat bhat <sharattbhat@gmail.com>
2022-11-27 01:15:32 +1100

    Add product controller

---
+++ Ecommerce.API/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductWarehouse.Business.Stores;
+
+namespace ProductWarehouse.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductStore _productStore;
+
+        public ProductController(IProductStore productStore)
+        {
+            _productStore = productStore ?? throw new ArgumentNullException(nameof(productStore));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> FindByCode([FromRoute] string id)
+        {
+            var result = await _productStore.FindById(id);
+
+            return Ok(result);
+        }
+    }
+}
